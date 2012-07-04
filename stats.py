dd8f4346ab774a2a12543cab0532467229b36b48
julia.ramos.beltrao@gmail.com <julia.ramos.beltrao@gmail.com@d3adaeae-a894-a328-183d-78ba4206af76>
2012-07-04 17:36:58 +0000

    Few more advances on compd.py and stats.py. New functions in the latter.

--- stats.py
+++ stats.py
@@ -2,41 +2,72 @@
 
 import math
 
-def mean1(sum, size):
-	return sum/size
+def sum(x):
+	s=0
+        for n in x:
+                s+=n
+        return s
 
-def mean2(x):
-	sum=0
-	for n in x:
-		sum+=n
-	return sum/len(x)
+def sqsum(x): #calculates the sum of the squares of the values
 
-def diff(x, y):
+def prod(x):
+	p=1
+        for val in x:
+                p *= val
+	return p	
 
-	dif=[]
-
-	if len(x) > len(y):
-		n = len(y)
-		d = len(x) - len(y)
-	elif len(x) < len(y):
-		n = len(x)
-		d = len(y) - len(x)
+def nroot(val, n):
+	if val == 1:
+		return 1
+	elif n == 2:
+		return sqrt(val)
 	else:
-		n = len(x)
-		d = 0
+		x0 = 0 
+		xk = sqrt(val)
 
-	for i in range(n):
-		dif.append(x[i]-y[i])
+		while (xk != x0):
+			x0 = xk
+			xk = x0 - ((pow(x0, n)-val)/n*pow(x0, n-1))
+	return xk
+	
+def average(ssum, ssize):
+	return ssum/ssize
 
-	return dif, d
-	
-	
+def gmean(x):
+	return nroot(prod(x))
+
 def var(sum1, sum2, size):
 	return (sum2-(sum1**2/size))/(size-1)
 
-def stdv(sum1, sum2, size):
+def stdv1(variance)
+	return sqrt(variance)
+
+def stdv2(sum1, sum2, size):
 	return sqrt(var(sum1, sum2, size))
 
 def conf(stdev, size):
-	#atribuicao de tstar vira aqui
+	#tstar attribution yet to be coded
 	return tstar*(stdev/sqrt(size))
+
+def ratio(val1 , val2):
+	return val1/val2
+
+def diff(x, y):
+
+        dif=[]
+
+        if len(x) > len(y):
+                n = len(y)
+                d = len(x) - len(y)
+        elif len(x) < len(y):
+                n = len(x)
+                d = len(y) - len(x)
+        else:
+                n = len(x)
+                d = 0
+
+        for i in range(n):
+                dif.append(x[i]-y[i])
+
+        return dif, d
+
