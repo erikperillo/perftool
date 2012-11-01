889ffbd564f55c54eac60e75b3a899795525ef46
julia.ramos.beltrao@gmail.com <julia.ramos.beltrao@gmail.com@d3adaeae-a894-a328-183d-78ba4206af76>
2012-11-01 19:32:18 +0000

    bug fixing and testing

--- trunk/plot.py
+++ trunk/plot.py
@@ -3,12 +3,13 @@
 import matplotlib.pyplot as plt
 
 # one-curve-per-revision-plot
-def time_vs_config_plot(x, y, e, cpg, dout, file):
+def time_vs_config_plot(x, y, e, cpg, l, dout, file):
 
 
 	n = len(x)
 	print "n: ", n
 
+	i=1
 	index=0
 	index_max=cpg
 
@@ -16,21 +17,24 @@ def time_vs_config_plot(x, y, e, cpg, dout, file):
 		while index < index_max:
 			if index >= n:
 				break
-			print "index: ", index
+			#print "index: ", index
 			print "time: ", y[index]
 			print "error: ",  e[index]
-			plt.errorbar([0,2,4,8], y[index], e[index], label="r"+str(x[index]))
+			plt.errorbar([0,2,4,8], y[index], e[index], label=l+str(x[index]))
 			index+=1
 			
-		print "index max: ", index_max
+		#print "index max: ", index_max
 		print "plot!"
 		plt.grid('on')
+		plt.margins(0.1, 0.1)
 		plt.suptitle(file, fontsize=15)
 		plt.xlabel('ntsm', fontsize=12)		
 		plt.ylabel('t(s)', fontsize=12)		
 		plt.legend(loc=4)
-		plt.savefig(dout + file + ".png")
-		#plt.show()
+		#plt.savefig(dout + file + str(i) + ".png")
+		#plt.close()
+		plt.show()
+		i = i + 1
 		index_max = index_max + cpg
 
 	
