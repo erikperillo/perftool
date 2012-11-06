e1fee6b192cbf3658ff34df9558418c73a228263
julia.ramos.beltrao@gmail.com <julia.ramos.beltrao@gmail.com@d3adaeae-a894-a328-183d-78ba4206af76>
2012-11-06 15:10:15 +0000

    New function on reportd module: reassemble data

--- trunk/plot.py
+++ trunk/plot.py
@@ -3,38 +3,44 @@
 import matplotlib.pyplot as plt
 
 # one-curve-per-revision-plot
-def time_vs_config_plot(x, y, e, cpg, l, dout, file):
+def time_vs_config_plot(x, y, e, leg, cpg, l1, l2, dout, file):
 
 
 	n = len(x)
 	print "n: ", n
 
-	i=1
 	index=0
 	index_max=cpg
 
 	while index < n:
+		i=-1
 		while index < index_max:
 			if index >= n:
 				break
-			#print "index: ", index
-			print "time: ", y[index]
-			print "error: ",  e[index]
-			plt.errorbar([0,2,4,8], y[index], e[index], label=l+str(x[index]))
+			print "index: ", index
+			print "time: ", len(y[index])
+			print "error: ",  len(e[index])
+			if l1 == 'r':
+				i = index
+			else:
+				i = i + 1
+				print "funfou!"
+			print 'i: ', i
+			print "leg: ", leg[i]
+			plt.errorbar(x, y[index], e[index], label=l1+str(leg[i]))
 			index+=1
 			
 		#print "index max: ", index_max
-		print "plot!"
 		plt.grid('on')
-		plt.margins(0.1, 0.1)
+		plt.margins(0.2, 0.2)
 		plt.suptitle(file, fontsize=15)
-		plt.xlabel('ntsm', fontsize=12)		
+		plt.xlabel(l2, fontsize=12)		
 		plt.ylabel('t(s)', fontsize=12)		
-		plt.legend(loc=4)
+		plt.legend(loc=8, ncol=cpg, mode="expand")
 		#plt.savefig(dout + file + str(i) + ".png")
 		#plt.close()
+		print "plot!"
 		plt.show()
-		i = i + 1
 		index_max = index_max + cpg
 
 	
