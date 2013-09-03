96b9e8a81d190729fc9379dd19c8509c454ce931
julia.ramos.beltrao@gmail.com <julia.ramos.beltrao@gmail.com@d3adaeae-a894-a328-183d-78ba4206af76>
2013-09-03 20:10:52 +0000

    New functions in module csv_file.py. (Testing still needed.)

--- trunk/csv_file.py
+++ trunk/csv_file.py
@@ -29,6 +29,45 @@ def write(file, line):
 		return 1
 	return 0
 	
-#TODO: escrever funcao
-#def read(file, line, col):
+#TODO: testar funcao
+def read(file, line='*', col='*'):
+	status=0
+	list=[]
+	try:
+		f = open(file, 'r')
+		if line=='*' and col=='*':
+			f.readline()
+			list=f.readlines()
+		else:
+			if line == '*':
+			f.readline()
+			temp=f.readlines()
+			for l in temp:
+				list.append(l[col-1])
 
+			elif col == '*':
+				list=f.readlines()[line]
+
+			else:
+				list=f.readlines()[line]
+				list=list.split()[col-1]	
+
+		f.close()
+	except IOError:
+		status=1 
+	return status, list
+
+#TODO: testar funcao
+def check(file):
+	status=0
+	try:
+		f = open(file, 'r')
+		lines = f.readlines()
+	except IOError:
+		return 1
+	ncol = len(line[0])
+	for line in lines:
+		if len(line) != ncol:
+			status=1
+			break
+	return status		
